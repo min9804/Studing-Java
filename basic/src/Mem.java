294f2ddc5e05e9a42b33225119b125b019536615
minteiro <dkdlel4763@naver.com>
2021-03-09 00:12:56 +0900

    studing java

---
+++ basic/src/Mem.java
@@ -0,0 +1,19 @@
+
+public class Mem {
+
+	public static void main(String[] args) {
+		int a;
+		int b;
+		double c;
+		String str;
+		int d =4;
+		
+		a = 3;
+		b = a +3;
+		b = b *3;
+		c = 4.5;
+		str = "a";
+		System.out.println(a);
+		System.out.println("a");
+	}
+}
